3a766ed5e048a0293ec8aeb90796adc9d38c231b
Arsen-Git <asermahambetov@gmail.com>
2023-12-14 20:02:59 +0100

    ADDED: input validation animation

--- src/components/Newsletter/Newsletter.jsx
+++ src/components/Newsletter/Newsletter.jsx
@@ -1,7 +1,35 @@
 import "./Newsletter.scss";
 import "../../global.scss";
 
+import { useState } from "react";
+
 export default function Newsletter() {
+  const [inputValue, setInputValue] = useState("");
+
+  const onValidate = (e) => {
+    if (
+      e.target.value !== "" &&
+      !e.target.value.match("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{1,63}$")
+    ) {
+      e.target.classList.add("newsletter__input-invalid");
+      e.target.parentNode.classList.add("newsletter__input__field-invalid");
+      document
+        .querySelector(".newsletter__input__field__error")
+        .classList.add("newsletter__input__field__error-active");
+    } else {
+      e.target.classList.remove("newsletter__input-invalid");
+      e.target.parentNode.classList.remove("newsletter__input__field-invalid");
+      document
+        .querySelector(".newsletter__input__field__error")
+        .classList.remove("newsletter__input__field__error-active");
+    }
+  };
+
+  const onType = (e) => {
+    onValidate(e);
+    setInputValue(e.target.value);
+  };
+
   return (
     <section className="newsletter__wrapper">
       <div className="newsletter__content">
@@ -9,15 +37,29 @@ export default function Newsletter() {
         <h3 className="newsletter__title">
           Stay up-to-date with what we're doing
         </h3>
-        <div className="newsletter__input">
-          <input
-            type="text"
-            name="newsletter"
-            id="newsletter"
-            placeholder="Enter your email adress"
-          />
-          <button className="btn__orange">Contact Us</button>
-        </div>
+        <form className="newsletter__input__form">
+          <div className="newsletter__input__field">
+            <input
+              type="email"
+              pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$"
+              name="newsletter"
+              id="newsletter"
+              value={inputValue}
+              onChange={onType}
+              placeholder="Enter your email adress"
+            />
+            <span className="newsletter__input__field__error">
+              Whoops, make sure it's an email.
+            </span>
+          </div>
+          <button
+            onClick={(e) => e.preventDefault()}
+            type="submit"
+            className="btn__orange"
+          >
+            Contact Us
+          </button>
+        </form>
       </div>
     </section>
   );
